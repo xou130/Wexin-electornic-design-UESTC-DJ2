eafec945df1dc16393e356480b8d80c03c733c4f
DC <dongqian43@outlook.com>
2015-04-13 19:11:45 +0800

    HARD_SPI "Hello world" Achieved
    在各种折腾后，终于能刷屏了！
    用了32的硬件SPI控制器，耗时3天（没有连击的几天除了周末）
    接下来：IIC 对at24c08 通讯，掉电保持数据
    或者：4路倒计时屏幕实现
    选择？

---
+++ Electronic Design/32 Host/USER/font.h
@@ -0,0 +1,12 @@
+#ifndef __FONT_H
+#define __FONT_H		
+extern  unsigned char code_image[];
+extern unsigned char code_hanzi[];
+extern unsigned char code_asc2_1608[1520];
+					  		 
+#endif  
+	 
+	 
+
+
+
