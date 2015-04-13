12262bab2dae2a745583a01a6b3d24b6b38d3935
DC <dongqian43@outlook.com>
2015-04-13 14:38:34 +0800

    USE_HardSPI
    开始换做硬件spi，但遇到flag不清零的问题

--- Electronic Design/32 Host/USER/main.c
+++ Electronic Design/32 Host/USER/main.c
@@ -3,73 +3,184 @@
 #define uint unsigned int
 
 void delayms(int m)
-{m*=10;while(m--);}
+{m*=1000;while(m--);}
 
  //LCD size
 #define LCD_W 240
 #define LCD_H 320
+#define LCD_DC GPIOB,GPIO_Pin_6
+#define LCD_CS GPIOB,GPIO_Pin_12
+#define LCD_REST GPIOB,GPIO_Pin_5
+  u16 BACK_COLOR=0xffff, POINT_COLOR=0x0000;
+void Lcd_Init(void); 
+void LCD_Clear(u16 Color);
+void Address_set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2);
+void LCD_WR_DATA8(u8);
+void LCD_WR_DATA(int da);
+void LCD_WR_REG(u8);
+
+void LCD_DrawPoint(u16 x,u16 y);//??
+void LCD_DrawPoint_big(u16 x,u16 y);//?????
+u16  LCD_ReadPoint(u16 x,u16 y); //??
+void Draw_Circle(u16 x0,u16 y0,u8 r);
+void LCD_DrawLine(u16 x1, u16 y1, u16 x2, u16 y2);
+void LCD_DrawRectangle(u16 x1, u16 y1, u16 x2, u16 y2);		   
+void LCD_Fill(u16 xsta,u16 ysta,u16 xend,u16 yend,u16 color);
+void LCD_ShowChar(u16 x,u16 y,u8 num,u8 mode);//??????
+void LCD_ShowNum(u16 x,u16 y,u32 num,u8 len);//????
+void LCD_Show2Num(u16 x,u16 y,u16 num,u8 len);//??2???
+void LCD_ShowString(u16 x,u16 y,const u8 *p);		 //???????,16??
+
+unsigned char asc2_1608[1520];
+			
+#define WHITE         	 0xFFFF
+#define BLACK         	 0x0000	  
+#define BLUE         	 0x001F  
+#define BRED             0XF81F
+#define GRED 			 0XFFE0
+#define GBLUE			 0X07FF
+#define RED           	 0xF800
+#define MAGENTA       	 0xF81F
+#define GREEN         	 0x07E0
+#define CYAN          	 0x7FFF
+#define YELLOW        	 0xFFE0
+#define BROWN 			 0XBC40 //??
+#define BRRED 			 0XFC07 //???
+#define GRAY  			 0X8430 //??
+//GUI??
+
+#define DARKBLUE      	 0X01CF	//???
+#define LIGHTBLUE      	 0X7D7C	//???  
+#define GRAYBLUE       	 0X5458 //???
+//?????PANEL??? 
+ 
+#define LIGHTGREEN     	 0X841F //???
+#define LGRAY 			 0XC618 //???(PANNEL),?????
+
+#define LGRAYBLUE        0XA651 //????(?????)
+#define LBBLUE           0X2B12 //????(???????)
+
+
+					  		 
+
+	 
 
 
 void LED_Init()
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG, ENABLE);
-	
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;			 //??LED?? D2
   	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   	GPIO_Init(GPIOG, &GPIO_InitStructure);
 }
 
 
-void LCD_Init()
+
+
+void LCD_SPI_Init()
 {
+	SPI_InitTypeDef SPI_InitStructure;
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOB,ENABLE);
+	
+	//SCK=B13
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	
-	GPIO_SetBits(GPIOB,GPIO_Pin_4);
-	GPIO_ResetBits(GPIOB,GPIO_Pin_4);
+	//MISO=B14
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_14;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	
-	GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-	GPIO_SetBits(GPIOB,GPIO_Pin_5);
+	//MOSI=B15
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	
 
-}//CS=B4,RST=B5,DC=B6,SCL=B7,SDA=B8
+	
+	//SPI Conf
+	RCC_APB2PeriphClockCmd(RCC_APB1Periph_SPI2,ENABLE);
+	SPI_InitStructure.SPI_Direction=SPI_Direction_2Lines_FullDuplex  ;
+	SPI_InitStructure.SPI_Mode=SPI_Mode_Master;
+	SPI_InitStructure.SPI_DataSize=SPI_DataSize_8b;
+	SPI_InitStructure.SPI_CPOL=SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA=SPI_CPHA_2Edge;
+	SPI_InitStructure.SPI_NSS=SPI_NSS_Soft;
+	SPI_InitStructure.SPI_BaudRatePrescaler=SPI_BaudRatePrescaler_2;
+	SPI_InitStructure.SPI_FirstBit=SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial=7;
+	SPI_Init(SPI2,&SPI_InitStructure);
+	SPI_Cmd(SPI2,ENABLE);
+	//SPI-CONF
+	
+	//CS=B12
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+		//RST=B5
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	//DC=B6
+		GPIO_InitStructure.GPIO_Pin=GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	
+	
+	#define CS_0() GPIO_ResetBits(GPIOB,GPIO_Pin_12)
+	#define CS_1() GPIO_SetBits(GPIOB,GPIO_Pin_12)
+	#define DC_0() GPIO_ResetBits(GPIOB,GPIO_Pin_6)
+	#define DC_1() GPIO_SetBits(GPIOB,GPIO_Pin_6)
+	#define RST_0() GPIO_ResetBits(GPIOB,GPIO_Pin_5)
+	#define RST_1() GPIO_SetBits(GPIOB,GPIO_Pin_5)
+	
+	RST_1();
+	CS_0();
+	
+}//RST=B5,,DC=B6,,CS=B12
 
-void LCD_Writ_Bus(char data)
+u8 LCD_Writ_Bus(u8 da)
 {
-	int cnt;
-	for(cnt=0;cnt<8;cnt++)
+	/*int cnt;
+	for(cnt=0;cnt<8;cnt++,da<<=1)
 	{
-		if((data<<cnt)&0x80)GPIO_SetBits(GPIOB,GPIO_Pin_8);
-			else GPIO_ResetBits(GPIOB,GPIO_Pin_8);
+		if(da&0x80){GPIO_SetBits(GPIOB,GPIO_Pin_8);}
+			else {GPIO_ResetBits(GPIOB,GPIO_Pin_8);}
 		GPIO_SetBits(GPIOB,GPIO_Pin_7);
+		GPIO_ResetBits(GPIOB,GPIO_Pin_7);
+	}*/
+	while(SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE)==RESET);  ////!!!!currently stack here
+	SPI_I2S_SendData(SPI2,da);
+	while(SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE)==RESET);
+	return SPI_I2S_ReceiveData(SPI2);
 	
-		GPIO_ResetBits(GPIOB,GPIO_Pin_7);	
-	}
 }
 
-void LCD_WR_DATA8(char da)
+
+void LCD_WR_DATA8(u8 da) 
 {
-	GPIO_SetBits(GPIOB,GPIO_Pin_6);
+    GPIO_SetBits(LCD_DC);
 	LCD_Writ_Bus(da);
-
-}
-
-void LCD_WR_DATA(int da)
+}  
+ void LCD_WR_DATA(int da)
 {
-  GPIO_SetBits(GPIOB,GPIO_Pin_6);
+    GPIO_SetBits(LCD_DC);
 	LCD_Writ_Bus(da>>8);
 	LCD_Writ_Bus(da);
-}
-
-void LCD_WR_REG(char da)	 
+}	  
+void LCD_WR_REG(u8 da)	 
 {
-  GPIO_ResetBits(GPIOB,GPIO_Pin_6);
+    GPIO_ResetBits(LCD_DC);
 	LCD_Writ_Bus(da);
 }
  void LCD_WR_REG_DATA(int reg,int da)
@@ -77,56 +188,27 @@ void LCD_WR_REG(char da)
     LCD_WR_REG(reg);
 	LCD_WR_DATA(da);
 }
-
 void Address_set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2)
 {  
    LCD_WR_REG(0x2a);
    LCD_WR_DATA8(x1>>8);
    LCD_WR_DATA8(x1);
    LCD_WR_DATA8(x2>>8);
-   LCD_WR_DATA8(x2); 
+   LCD_WR_DATA8(x2);
+  
    LCD_WR_REG(0x2b);
    LCD_WR_DATA8(y1>>8);
    LCD_WR_DATA8(y1);
    LCD_WR_DATA8(y2>>8);
    LCD_WR_DATA8(y2);
-   LCD_WR_REG(0x2C);
-}
 
-void LCD_Clear(u16 Color)
-{
-	u8 VH,VL;
-	u16 i,j;
-	VH=Color>>8;
-	VL=Color;	
-	Address_set(0,0,LCD_W-1,LCD_H-1);
-    for(i=0;i<LCD_W;i++)
-	 {
-	  for (j=0;j<LCD_H;j++)
-	   	{
-       LCD_WR_DATA8(VH);
-			 LCD_WR_DATA8(VL);	
-	    }
-	  }
+   LCD_WR_REG(0x2C);
+
 }
 void LCD_fInit(void)
 {
 
-
-   	GPIO_SetBits(GPIOB,GPIO_Pin_4);
-
-	   LCD_WR_REG_DATA(0,0);
-
-    GPIO_SetBits(GPIOB,GPIO_Pin_5);
-    delayms(5);	
-	GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-	delayms(5);
-	GPIO_SetBits(GPIOB,GPIO_Pin_5);
-	GPIO_SetBits(GPIOB,GPIO_Pin_4);
-	delayms(5);
-	GPIO_ResetBits(GPIOB,GPIO_Pin_4);  
-
-		LCD_WR_REG(0xCB);  
+				LCD_WR_REG(0xCB);  
         LCD_WR_DATA8(0x39); 
         LCD_WR_DATA8(0x2C); 
         LCD_WR_DATA8(0x00); 
@@ -231,18 +313,259 @@ void LCD_fInit(void)
         LCD_WR_REG(0x2c); 
 
 }
+//????
+//Color:???????
+void LCD_Clear(u16 Color)
+{
+	u8 VH,VL;
+	u16 i,j;
+	VH=Color>>8;
+	VL=Color;	
+	Address_set(0,0,LCD_W-1,LCD_H-1);
+    for(i=0;i<LCD_W;i++)
+	 {
+	  for (j=0;j<LCD_H;j++)
+	   	{
+        	 LCD_WR_DATA8(VH);
+			 LCD_WR_DATA8(VL);	
+
+	    }
+
+	  }
+}
+
+//??
+//POINT_COLOR:?????
+void LCD_DrawPoint(u16 x,u16 y)
+{
+	Address_set(x,y,x,y);//?????? 
+	LCD_WR_DATA(POINT_COLOR); 	    
+} 	 
+//?????
+//POINT_COLOR:?????
+void LCD_DrawPoint_big(u16 x,u16 y)
+{
+	LCD_Fill(x-1,y-1,x+1,y+1,POINT_COLOR);
+} 
+//????????????
+//????:
+//  (xend-xsta)*(yend-ysta)
+void LCD_Fill(u16 xsta,u16 ysta,u16 xend,u16 yend,u16 color)
+{          
+	u16 i,j; 
+	Address_set(xsta,ysta,xend,yend);      //?????? 
+	for(i=ysta;i<=yend;i++)
+	{													   	 	
+		for(j=xsta;j<=xend;j++)LCD_WR_DATA(color);//?????? 	    
+	} 					  	    
+}  
+//??
+//x1,y1:????
+//x2,y2:????  
+void LCD_DrawLine(u16 x1, u16 y1, u16 x2, u16 y2)
+{
+	u16 t; 
+	int xerr=0,yerr=0,delta_x,delta_y,distance; 
+	int incx,incy,uRow,uCol; 
+
+	delta_x=x2-x1; //?????? 
+	delta_y=y2-y1; 
+	uRow=x1; 
+	uCol=y1; 
+	if(delta_x>0)incx=1; //?????? 
+	else if(delta_x==0)incx=0;//??? 
+	else {incx=-1;delta_x=-delta_x;} 
+	if(delta_y>0)incy=1; 
+	else if(delta_y==0)incy=0;//??? 
+	else{incy=-1;delta_y=-delta_y;} 
+	if( delta_x>delta_y)distance=delta_x; //????????? 
+	else distance=delta_y; 
+	for(t=0;t<=distance+1;t++ )//???? 
+	{  
+		LCD_DrawPoint(uRow,uCol);//?? 
+		xerr+=delta_x ; 
+		yerr+=delta_y ; 
+		if(xerr>distance) 
+		{ 
+			xerr-=distance; 
+			uRow+=incx; 
+		} 
+		if(yerr>distance) 
+		{ 
+			yerr-=distance; 
+			uCol+=incy; 
+		} 
+	}  
+}    
+//???
+void LCD_DrawRectangle(u16 x1, u16 y1, u16 x2, u16 y2)
+{
+	LCD_DrawLine(x1,y1,x2,y1);
+	LCD_DrawLine(x1,y1,x1,y2);
+	LCD_DrawLine(x1,y2,x2,y2);
+	LCD_DrawLine(x2,y1,x2,y2);
+}
+//??????????????
+//(x,y):???
+//r    :??
+void Draw_Circle(u16 x0,u16 y0,u8 r)
+{
+	int a,b;
+	int di;
+	a=0;b=r;	  
+	di=3-(r<<1);             //??????????
+	while(a<=b)
+	{
+		LCD_DrawPoint(x0-b,y0-a);             //3           
+		LCD_DrawPoint(x0+b,y0-a);             //0           
+		LCD_DrawPoint(x0-a,y0+b);             //1       
+		LCD_DrawPoint(x0-b,y0-a);             //7           
+		LCD_DrawPoint(x0-a,y0-b);             //2             
+		LCD_DrawPoint(x0+b,y0+a);             //4               
+		LCD_DrawPoint(x0+a,y0-b);             //5
+		LCD_DrawPoint(x0+a,y0+b);             //6 
+		LCD_DrawPoint(x0-b,y0+a);             
+		a++;
+		//??Bresenham????     
+		if(di<0)di +=4*a+6;	  
+		else
+		{
+			di+=10+4*(a-b);   
+			b--;
+		} 
+		LCD_DrawPoint(x0+a,y0+b);
+	}
+} 
+//???????????
+
+//num:??????:" "--->"~"
+//mode:????(1)???????(0)
+//???????????
+
+//num:??????:" "--->"~"
+
+//mode:????(1)???????(0)
+void LCD_ShowChar(u16 x,u16 y,u8 num,u8 mode)
+{
+    u8 temp;
+    u8 pos,t;
+	u16 x0=x;
+	u16 colortemp=POINT_COLOR;      
+    if(x>LCD_W-16||y>LCD_H-16)return;	    
+	//????		   
+	num=num-' ';//???????
+	Address_set(x,y,x+8-1,y+16-1);      //?????? 
+	if(!mode) //?????
+	{
+		for(pos=0;pos<16;pos++)
+		{ 
+			temp=asc2_1608[(u16)num*16+pos];		 //??1608??
+			for(t=0;t<8;t++)
+		    {                 
+		        if(temp&0x01)POINT_COLOR=colortemp;
+				else POINT_COLOR=BACK_COLOR;
+				LCD_WR_DATA(POINT_COLOR);	
+				temp>>=1; 
+				x++;
+		    }
+			x=x0;
+			y++;
+		}	
+	}else//????
+	{
+		for(pos=0;pos<16;pos++)
+		{
+		    temp=asc2_1608[(u16)num*16+pos];		 //??1608??
+			for(t=0;t<8;t++)
+		    {                 
+		        if(temp&0x01)LCD_DrawPoint(x+t,y+pos);//????     
+		        temp>>=1; 
+		    }
+		}
+	}
+	POINT_COLOR=colortemp;	    	   	 	  
+}   
+//m^n??
+u32 mypow(u8 m,u8 n)
+{
+	u32 result=1;	 
+	while(n--)result*=m;    
+	return result;
+}			 
+//??2???
+//x,y :????	 
+//len :?????
+//color:??
+//num:??(0~4294967295);	
+void LCD_ShowNum(u16 x,u16 y,u32 num,u8 len)
+{         	
+	u8 t,temp;
+	u8 enshow=0;
+	num=(u16)num;
+	for(t=0;t<len;t++)
+	{
+		temp=(num/mypow(10,len-t-1))%10;
+		if(enshow==0&&t<(len-1))
+		{
+			if(temp==0)
+			{
+				LCD_ShowChar(x+8*t,y,' ',0);
+				continue;
+			}else enshow=1; 
+		 	 
+		}
+	 	LCD_ShowChar(x+8*t,y,temp+48,0); 
+	}
+} 
+//??2???
+//x,y:????
+//num:??(0~99);	 
+void LCD_Show2Num(u16 x,u16 y,u16 num,u8 len)
+{         	
+	u8 t,temp;						   
+	for(t=0;t<len;t++)
+	{
+		temp=(num/mypow(10,len-t-1))%10;
+	 	LCD_ShowChar(x+8*t,y,temp+'0',0); 
+	}
+} 
+//?????
+//x,y:????  
+//*p:???????
+//?16??
+void LCD_ShowString(u16 x,u16 y,const u8 *p)
+{         
+    while(*p!='\0')
+    {       
+        if(x>LCD_W-16){x=0;y+=16;}
+        if(y>LCD_H-16){y=x=0;}
+        LCD_ShowChar(x,y,*p,0);
+        x+=8;
+        p++;
+    }  
+}
 
 
 
 int main(void)
 {
-	LCD_Init();
+	LCD_SPI_Init();
 	LED_Init();
+	GPIO_SetBits(GPIOG,GPIO_Pin_14);
+	delayms(500);
+	GPIO_ResetBits(GPIOG,GPIO_Pin_14);
 	LCD_fInit();
-	LCD_Clear(0x0000);
 	GPIO_SetBits(GPIOG,GPIO_Pin_14);
 	
 	
 	
 	LCD_Clear(0xAAAA);
+	GPIO_ResetBits(GPIOG,GPIO_Pin_14);
+	while(1){
+		GPIO_SetBits(GPIOG,GPIO_Pin_14);
+		
+		delayms(5000);	LCD_DrawLine(0,0,100,100);
+		GPIO_ResetBits(GPIOG,GPIO_Pin_14);
+	
+delayms(10000);}
 }
